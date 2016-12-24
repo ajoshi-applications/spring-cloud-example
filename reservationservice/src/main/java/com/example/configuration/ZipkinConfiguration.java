1013f591ef5eb0f411d990f588410fe7d3dbb22d
Artur <arturjoshi.mailbox@gmail.com>
2016-12-24 18:26:26 +0200

    Added zipkin

---
+++ reservationservice/src/main/java/com/example/configuration/ZipkinConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.configuration;
+
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by ajoshi on 24-Dec-16.
+ */
+@Configuration
+public class ZipkinConfiguration {
+    @Bean
+    Sampler sampler() {
+        return new Sampler() {
+            @Override
+            public boolean isSampled(Span span) {
+                return true;
+            }
+        };
+    }
+}
