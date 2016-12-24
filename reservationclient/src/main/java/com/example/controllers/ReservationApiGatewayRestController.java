92de7660d244981fd5381387852ed18991e35915
Artur <arturjoshi.mailbox@gmail.com>
2016-12-24 16:48:55 +0200

    stable

---
+++ reservationclient/src/main/java/com/example/controllers/ReservationApiGatewayRestController.java
@@ -0,0 +1,53 @@
+package com.example.controllers;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ajoshi on 24-Dec-16.
+ */
+@RestController
+@RequestMapping("/reservations")
+public class ReservationApiGatewayRestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Collection<String> getReservationNamesFallback() {
+        return new ArrayList<>();
+    }
+
+    @HystrixCommand(fallbackMethod = "getReservationNamesFallback")
+    @RequestMapping(method = RequestMethod.GET, value = "/names")
+    public Collection<String> getReservationNames() {
+        ParameterizedTypeReference<Resources<Reservation>> ptr = new ParameterizedTypeReference<Resources<Reservation>>() {
+        };
+        ResponseEntity<Resources<Reservation>> entity = this.restTemplate
+                .exchange("http://localhost:8080/reservation-service/reservations", HttpMethod.GET, null, ptr);
+        return entity.getBody()
+                .getContent()
+                .stream()
+                .map(Reservation::getReservationName)
+                .collect(Collectors.toList());
+    }
+}
+
+class Reservation {
+    private String reservationName;
+
+    public String getReservationName() {
+        return reservationName;
+    }
+}
